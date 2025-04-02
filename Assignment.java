89e759990867705bd5495b5fe632fe1fe4cf16c9
SamplerNix <nikhilsingh7534@gmail.com>
2025-04-02 23:56:52 +0530

    Added question 8,9,10

--- Assignment.java
+++ Assignment.java
@@ -1,5 +1,58 @@
 import java.util.Scanner;
+//question10
+class Animal {
+    String name;
+    public Animal(String name) {
+        this.name = name;
+    }
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+    public void sleep() {
+        System.out.println(name + " is sleeping.");
+    }
+}
+// Child class that inherits from Animal
+class Dog extends Animal {
 
+    // Constructor for Dog
+    public Dog(String name) {
+        // Calling the parent class (Animal) constructor
+        super(name);
+    }
+
+    // Additional method specific to Dog
+    public void bark() {
+        System.out.println(name + " is barking.");
+    }
+}
+
+//question 9
+class methodoverloading{
+    public static void method(int n){
+        System.out.println("Square of number is:"+n*n);
+    }
+        public static void method(float n){
+        System.out.println("This floating number:"+n);
+    }
+        public static void method(int n,int m){
+        System.out.println("These are the two number which are in int form:"+n+","+m);
+    }
+}
+//question 8
+class student{
+    public static void student(){
+    String name="Nikhil";
+    int roll_no=78;
+    String[] Marks={"Math","English","Science","History"};
+    int[] marks={78,86,89,90};
+    System.out.println("Name:"+name);
+    System.out.println("Rollno:"+roll_no);
+    for(int i=0;i<4;i++){
+        System.out.println(Marks[i]+":"+marks[i]);
+    }
+}
+}
 class questioon6 {
 
     public static void isPrime(int n) {
@@ -47,9 +100,18 @@ class question7 {
 class Assignment {
 
     public static void main(String[] args) {
-        //  System.out.println("Hello worlds"); 
+         System.out.println("Hello worlds"); 
         questioon6.isPrime(9);
         question7.factorial(5);
         question7.oddeve(5);
+        // //question 8
+        student.student();
+        //question 9//This is method overloading , use , int and (int,int)
+        methodoverloading.method((float)2.3);
+        //question10
+        Dog dog=new Dog("hello");
+        dog.eat();
+        dog.sleep();
+        dog.bark();
     }
 }
